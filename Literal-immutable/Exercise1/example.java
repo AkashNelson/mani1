96b888c3d4c00dbe132b27292da5fa1cd75f55bd
AkashNelson <akashnelson991@gmail.com>
2021-12-09 15:32:53 +0530

    ghj

--- Literal-immutable/Exercise1/example.java
+++ Literal-immutable/Exercise1/example.java
@@ -8,5 +8,7 @@ public static void main(String[] args) {
 			System.out.println(System.identityHashCode(s2));
 			String concat = s1.concat(s2);
 			System.out.println(System.identityHashCode(concat));
+			System.out.println("123");
+			
 }
 }
