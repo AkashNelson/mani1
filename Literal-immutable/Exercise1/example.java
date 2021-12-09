4df298e063ea6108d047687263184bef061c04bb
AkashNelson <akashnelson991@gmail.com>
2021-12-09 14:42:47 +0530

    hi

---
+++ Literal-immutable/Exercise1/example.java
@@ -0,0 +1,12 @@
+package Exercise1;
+
+public class example {
+public static void main(String[] args) {
+			String s1=" ";
+			System.out.println(System.identityHashCode(s1));
+			String s2="dog";
+			System.out.println(System.identityHashCode(s2));
+			String concat = s1.concat(s2);
+			System.out.println(System.identityHashCode(concat));
+}
+}
